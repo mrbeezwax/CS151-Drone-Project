d43799234e2197a1f7f3709a6e45d848cac5387f
Juju Liao <liaojulian@gmail.com>
2018-11-19 18:00:18 -0800

    More classes
    According to drone.pdf

---
+++ DroneGame.java
@@ -0,0 +1,10 @@
+public class DroneGame {
+    public void startGame() {
+        // Starts a timer/stopwatch
+        // Creates the game window
+        // Creates an instance of scoreboard
+        // Count points and lives
+    }
+
+
+}
