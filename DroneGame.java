73ec8b508875e9b6b2552793bb008f93b77cc846
Juju Liao <liaojulian@gmail.com>
2018-11-19 19:22:50 -0800

    Added windows and more classes

--- DroneGame.java
+++ DroneGame.java
@@ -1,10 +1,26 @@
+import javax.swing.*;
+
 public class DroneGame {
+    // Starts a stopwatch
+    // Creates an instance of scoreboard
+    // Count points and lives
+    // Manage collisions (Keep track of drone location and airplanes locations
     public void startGame() {
-        // Starts a timer/stopwatch
-        // Creates the game window
-        // Creates an instance of scoreboard
-        // Count points and lives
+        // Creates game window
+        // Displays Play Button and Configurations Button
+        JFrame gameWindow = new JFrame("Drone Project");
+        gameWindow.setSize(800, 400);
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // User clicks on configuration
+
+        // User clicks on play
+        // Initialize scoreboard, Drone, Airplanes, MovingPlain, Stopwatch...
     }
 
+    public void setConfigurations() {
+        JFrame gameWindow = new JFrame("Configurations");
+        gameWindow.setSize(300, 300);
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
 }
