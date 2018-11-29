8b7479093159bb703afd31cb457edcd839e183dc
Juju Liao <liaojulian@gmail.com>
2018-11-28 23:01:55 -0800

    Added moving airplane
    Test with main method in AirplaneTester

--- Airplane.java
+++ Airplane.java
@@ -1,29 +1,35 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
+import java.awt.*;
 
-public class Airplane extends JFrame {
-    private MoveableShape shape;
-    private ShapeIcon icon;
+public class Airplane extends JComponent {
+    private PlaneShape shape;
     private double x = -250;
     private double y = 0;
 
     public Airplane() {
         shape = new PlaneShape(x, y, 100);
-        icon = new ShapeIcon(shape, 775, 100);
-        JLabel label = new JLabel(icon);
-
-        add(label);
-
     }
 
-    public void actionPerformed(ActionEvent e) {
-
+    public void moveLeft() {
+        shape.right();
     }
 
-    public static void main(String[] args) {
-        Airplane frame = new Airplane();
-        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-        frame.setSize(800, 800);
-        frame.setVisible(true);
+    public void moveRight() {
+        shape.left();
     }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        shape.draw(g2);
+    }
+
+//    public static void main(String[] args) {
+//        JFrame testFrame = new JFrame();
+//        Airplane airplane = new Airplane();
+//        testFrame.setDefaultCloseOperation(testFrame.EXIT_ON_CLOSE);
+//        testFrame.setSize(800, 800);
+//        testFrame.setVisible(true);
+//        JLabel label = new JLabel(airplane.getIcon());
+//        testFrame.add(label);
+//    }
 }
--- PlaneShape.java
+++ PlaneShape.java
@@ -1,46 +1,43 @@
 import java.awt.*;
 import java.awt.geom.*;
-import java.util.*;
-import java.awt.event.*;
-import javax.swing.*;
 
 /**
  * A car that can be moved around.
  */
 public class PlaneShape implements MoveableShape {
-	private double x;
-	private double y;
-	private double width;
+    private double x;
+    private double y;
+    private double width;
 
-	/**
-	 * Constructs a car item.
-	 *
-	 * @param x     the left of the bounding rectangle
-	 * @param y     the top of the bounding rectangle
-	 * @param width the width of the bounding rectangle
-	 */
+    /**
+     * Constructs a car item.
+     *
+     * @param x     the left of the bounding rectangle
+     * @param y     the top of the bounding rectangle
+     * @param width the width of the bounding rectangle
+     */
 
-	public PlaneShape(double x, double y, double width) {
-		this.x = x + 25;
-		this.y = y;
-		this.width = width;
-	}
+    public PlaneShape(double x, double y, double width) {
+        this.x = x + 25;
+        this.y = y;
+        this.width = width;
+    }
 
-	/*
-	The x is flipped here because the scale method used in drawing the image flips the image and therefore, flips the x-axis
-	 */
-	@Override
-	public void left() {
-		x = x + 15;
-	}
+    /*
+    The x is flipped here because the scale method used in drawing the image flips the image and therefore, flips the x-axis
+     */
+    @Override
+    public void left() {
+        x = x + 15;
+    }
 
     /*
     The x is flipped here as well
      */
     @Override
-	public void right() {
-		x = x - 15;
-	}
+    public void right() {
+        x = x - 15;
+    }
 
     @Override
     public void down() {
@@ -54,28 +51,28 @@ public class PlaneShape implements MoveableShape {
 
     public void draw(Graphics2D g2) {
 
-		Rectangle2D.Double body = new Rectangle2D.Double(x, y + width / 6, width - 1, width / 6);
+        Rectangle2D.Double body = new Rectangle2D.Double(x, y + width / 6, width - 1, width / 6);
 
-		QuadCurve2D.Double headCurve2 = new QuadCurve2D.Double(x + width, y + width / 3 - 1, x + width + width / 3,
-				y + width / 3, x + width + width / 3, y + width / 3 - 5);
+        QuadCurve2D.Double headCurve2 = new QuadCurve2D.Double(x + width, y + width / 3 - 1, x + width + width / 3,
+                y + width / 3, x + width + width / 3, y + width / 3 - 5);
 
-		QuadCurve2D.Double headCurve = new QuadCurve2D.Double(x + width, y + width / 6, x + width + width / 3,
-				y + width / 6, x + width + width / 3, y + width / 3 - 5);
+        QuadCurve2D.Double headCurve = new QuadCurve2D.Double(x + width, y + width / 6, x + width + width / 3,
+                y + width / 6, x + width + width / 3, y + width / 3 - 5);
 
-		Line2D.Double tailLine2 = new Line2D.Double(x - 20, y - 10, x - 15, y - 10);
+        Line2D.Double tailLine2 = new Line2D.Double(x - 20, y - 10, x - 15, y - 10);
 
-		QuadCurve2D.Double tailCurve = new QuadCurve2D.Double(x, y + width / 3 - 1, x - 18, y + width / 3 - 2, x - 20,
-				y - 10);
+        QuadCurve2D.Double tailCurve = new QuadCurve2D.Double(x, y + width / 3 - 1, x - 18, y + width / 3 - 2, x - 20,
+                y - 10);
 
-		Line2D.Double tailLine3 = new Line2D.Double(x - 15, y - 10, x, y + width / 6 - 1);
+        Line2D.Double tailLine3 = new Line2D.Double(x - 15, y - 10, x, y + width / 6 - 1);
 
-		Line2D.Double rightWing1 = new Line2D.Double(x + 45, y + width / 3 - 10, x + 25, y + 40);
+        Line2D.Double rightWing1 = new Line2D.Double(x + 45, y + width / 3 - 10, x + 25, y + 40);
 
-		Line2D.Double rightWing2 = new Line2D.Double(x + 45, y + width / 3 - 10, x + 65, y + width / 3 - 10);
+        Line2D.Double rightWing2 = new Line2D.Double(x + 45, y + width / 3 - 10, x + 65, y + width / 3 - 10);
 
-		Line2D.Double rightWing3 = new Line2D.Double(x + 25, y + 40, x + 35, y + 40);
+        Line2D.Double rightWing3 = new Line2D.Double(x + 25, y + 40, x + 35, y + 40);
 
-		Line2D.Double rightWing4 = new Line2D.Double(x + 35, y + 40, x + 65, y + width / 3 - 10);
+        Line2D.Double rightWing4 = new Line2D.Double(x + 35, y + 40, x + 65, y + width / 3 - 10);
 
         g2.scale(-1,1);
         g2.draw(body);
@@ -89,6 +86,6 @@ public class PlaneShape implements MoveableShape {
         g2.draw(rightWing3);
         g2.draw(rightWing4);
 
-	}
+    }
 
 }
